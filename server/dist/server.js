a7f4b1b9dcdb6ebca71afdbf958a9f1ef38c776b
sieke13 <gera.ave2@gmail.com>
2025-02-28 16:31:21 -0600

    idk

--- server/dist/server.js
+++ server/dist/server.js
@@ -3,8 +3,11 @@ import { ApolloServer } from 'apollo-server-express';
 import path from 'path';
 import typeDefs from './schemas/typeDefs.js';
 import resolvers from './schemas/resolvers.js';
-import { authMiddleware } from './services/auth'; // Asegúrate de que authMiddleware esté tipado
-import db from './config/connection'; // Asegúrate de que db esté tipado
+import { authMiddleware } from './services/auth.js'; // Asegúrate de que authMiddleware esté tipado
+import db from './config/connection.js'; // Asegúrate de que db esté tipado
+import { fileURLToPath as originalFileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Crear una instancia de Apollo Server con tipos
@@ -33,3 +36,6 @@ db.once('open', () => {
         console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
     });
 });
+function fileURLToPath(url) {
+    return originalFileURLToPath(url);
+}
