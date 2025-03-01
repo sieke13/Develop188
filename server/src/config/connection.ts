f7b4707efd0bf059d867ace31aeb676f6f0ed871
sieke13 <gera.ave2@gmail.com>
2025-02-28 18:14:37 -0600

    hereigoagain

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://geraave2:gerardo123@cluster0.iqwly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 export default mongoose.connection;
